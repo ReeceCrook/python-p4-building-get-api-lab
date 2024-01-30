9846f6cc6e1563ad7e8668367fa60dd1cfdbb99b
ReeceCrook <reececrook16@gmail.com>
2024-01-30 16:14:58 -0700

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -18,21 +18,37 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route('/bakeries', methods=['GET'])
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_data = [
+        {'id': bakery.id, 'name': bakery.name, 'created_at': bakery.created_at, 'updated_at': bakery.updated_at}
+        for bakery in bakeries
+    ]
+    return jsonify(bakery_data)
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET'])
 def bakery_by_id(id):
-    return ''
+    bakery = db.session.get(Bakery, id)
+    bakery_data = {'id': bakery.id, 'name': bakery.name, 'created_at': bakery.created_at, 'updated_at': bakery.updated_at}
+    
+    return jsonify(bakery_data)
 
-@app.route('/baked_goods/by_price')
+@app.route('/baked_goods/by_price', methods=['GET'])
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_data = [
+        {'id': baked_good.id, 'name': baked_good.name, 'price': baked_good.price, 'created_at': baked_good.created_at, 'updated_at': baked_good.updated_at}
+        for baked_good in baked_goods
+    ]
+    return jsonify(baked_goods_data)
 
-@app.route('/baked_goods/most_expensive')
+@app.route('/baked_goods/most_expensive', methods=['GET'])
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_goods_data = {'id': baked_goods.id, 'name': baked_goods.name, 'price': baked_goods.price, 'created_at': baked_goods.created_at, 'updated_at': baked_goods.updated_at}
+    
+    return jsonify(baked_goods_data)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
